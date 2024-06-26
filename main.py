4dfc4b4dd3aa48cf88a7db4ec9489c299d9d0817
Artemka1806 <tyschenkoartem2580@gmail.com>
2024-06-26 14:37:17 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
+import asyncio
 import logging
 from os import getenv
 import sys
 from typing import Type
-from contextlib import asynccontextmanager
 
 from aiogram import Bot, Dispatcher, html, F, flags
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart, Command, MagicData
-from aiogram.types import Message, Update
+from aiogram.types import Message
 from mongoengine import connect
 from dotenv import load_dotenv
-from fastapi import FastAPI
-from fastapi.requests import Request
 
 from middlewares import auth, extra_data, menu
 from handlers import login, attendance
@@ -36,31 +34,6 @@ dp.include_routers(login.router, attendance.router)
 bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
 connect(host=MONGO_URI)
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-	await bot.set_webhook(
-		url=WEBHOOK_URL,
-		allowed_updates=dp.resolve_used_update_types(),
-		drop_pending_updates=True
-	)
-	yield
-	await bot.delete_webhook()
-
-
-app = FastAPI(lifespan=lifespan)
-
-
-@app.post(WEBHOOK_PATH)
-async def bot_webhook(request: Request) -> None:
-	update = Update.model_validate(await request.json(), context={"bot": bot})
-	await dp.feed_update(bot, update)
-
-
-@app.get("/")
-def main_web_handler():
-	return "Everything ok!"
 
 
 @dp.message(CommandStart(), MagicData(F.user))
@@ -91,5 +64,10 @@ I wanna share it with you
 I feel like a millionaire""")
 
 
+async def main():
+    await dp.start_polling(bot)
+
+
 if __name__ == "__main__":
 	logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+	asyncio.run(main())
