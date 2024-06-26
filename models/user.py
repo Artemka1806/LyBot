7c94730aefe149578a863535ebd58afb527d1bbb
Artemka1806 <tyschenkoartem2580@gmail.com>
2024-06-26 13:32:15 +0300

    Initial commit

--- models/user.py
+++ models/user.py
@@ -10,5 +10,5 @@ class User(Document):
 	email = EmailField(required=True)
 	group = StringField(max_length=4, required=True)
 	role = DecimalField(min_value=0, max_value=2, default=0)
-	status = DecimalField(min_value=0, max_value=3)
+	status = DecimalField(min_value=0, max_value=3, default=3)
 	created_at = DateTimeField(default=datetime.datetime.utcnow)
