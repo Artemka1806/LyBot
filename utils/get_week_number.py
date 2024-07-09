a11ad8d0ca70f4ce8e7f3ac6fb14dd22602cdc1d
Artemka1806 <tyschenkoartem2580@gmail.com>
2024-07-09 21:17:42 +0300

    Fixed get_week_number function

--- utils/get_week_number.py
+++ utils/get_week_number.py
@@ -5,4 +5,4 @@ def get_week_number(start_date: datetime):
 	current_date = datetime.now()
 	delta = current_date - start_date
 	weeks = delta.days / 7
-	return int(weeks % 4) - 1
+	return int(weeks % 4)
