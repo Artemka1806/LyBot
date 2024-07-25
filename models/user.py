78265672e59e304b67c79fe069f791cca85ce633
Artemka1806 <tyschenkoartem2580@gmail.com>
2024-07-25 10:36:40 +0300

    Added enums

--- models/user.py
+++ models/user.py
@@ -1,6 +1,13 @@
 import datetime
+from enum import Enum
 
 from mongoengine import *
+
+
+class Role(Enum):
+	USER = 0
+	MANAGER = 1
+	ADMIN = 2
 
 
 class User(Document):
@@ -9,6 +16,6 @@ class User(Document):
 	username = StringField(max_length=32)
 	email = EmailField(required=True)
 	group = StringField(max_length=4, required=True)
-	role = DecimalField(min_value=0, max_value=2, default=0)
+	role = EnumField(Role, default=Role.USER)
 	status = DecimalField(min_value=0, max_value=3, default=3)
 	created_at = DateTimeField(default=datetime.datetime.utcnow)
